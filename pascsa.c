5020e128d6bce6945f04b3eb1e7c8196826369da
e2401846 <tolahash1@gmail.com>
2025-10-28 09:26:46 +0200

    delete k

--- pascsa.c
+++ pascsa.c
@@ -10,11 +10,6 @@ long long factorial(int n) {
     return f;
 }
 
-// compute n choose k
-long long n_choose_k(int n, int k) {
-    return factorial(n) / (factorial(k) * factorial(n - k));
-}
-
 // declined Pascal triangle
 void declined_pascal(int n) {
     for (int i = 0; i < n; i++) {
